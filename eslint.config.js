1549f02e5c698bdf8b564595428b259cfc93c8fb
Jason Garber <jason@sixtwothree.org>
2023-10-15 20:29:07 -0400

    Remove Jasmine test suite

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,5 @@
 const config = require('@jgarber/eslint-config');
-const globals = require('globals');
 
 module.exports = [
-  ...config,
-  {
-    files: ['spec/**/*[sS]pec.?(m)js'],
-    languageOptions: {
-      globals: {
-        ...globals.jasmine
-      }
-    }
-  }
+  ...config
 ];
